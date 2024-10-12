719e08bab9695de35d7726f55c72d034293e460b
mikaelvz <mikael@mvz.fr>
2024-10-12 19:51:57 +0200

    SOF and UCM correct paths

--- build.sh
+++ build.sh
@@ -18,13 +18,17 @@ cp /tmp/cros-keyboard-map/local-overrides.quirks /etc/libinput/local-overrides.q
 
 # Install SOF
 git clone https://github.com/WeirdTreeThing/chromebook-linux-audio /tmp/chromebook-linux-audio
-cp -r /tmp/chromebook-linux-audio/conf/sof/tplg /lib/firmware/intel/sof-tplg
+cp -r /tmp/chromebook-linux-audio/conf/sof/tplg/* /lib/firmware/intel/sof-tplg/
 
 # Install UCM configuration
 git clone https://github.com/WeirdTreeThing/chromebook-ucm-conf /tmp/chromebook-ucm-conf
-cp -r /tmp/chromebook-ucm-conf/cml /usr/share/alsa/ucm2/conf.d
-cp -r /tmp/chromebook-ucm-conf/common /usr/share/alsa/ucm2/common
-cp -r /tmp/chromebook-ucm-conf/codecs /usr/share/alsa/ucm2/codecs
-cp -r /tmp/chromebook-ucm-conf/platforms /usr/share/alsa/ucm2/platforms
-cp -r /tmp/chromebook-ucm-conf/sof-rt5682 /usr/share/alsa/ucm2/conf.d/sof-rt5682
-cp -r /tmp/chromebook-ucm-conf/sof-cs42l42 /usr/share/alsa/ucm2/conf.d/sof-cs42l42
+cp -r /tmp/chromebook-ucm-conf/cml/* /usr/share/alsa/ucm2/conf.d
+cp -r /tmp/chromebook-ucm-conf/common/* /usr/share/alsa/ucm2/common
+cp -r /tmp/chromebook-ucm-conf/codecs/* /usr/share/alsa/ucm2/codecs
+cp -r /tmp/chromebook-ucm-conf/platforms/* /usr/share/alsa/ucm2/platforms
+
+mkdir -p /usr/share/alsa/ucm2/conf.d/sof-rt5682
+cp -r /tmp/chromebook-ucm-conf/sof-rt5682/* /usr/share/alsa/ucm2/conf.d/sof-rt5682
+
+mkdir -p /usr/share/alsa/ucm2/conf.d/sof-cs42l42
+cp -r /tmp/chromebook-ucm-conf/sof-cs42l42/* /usr/share/alsa/ucm2/conf.d/sof-cs42l42
