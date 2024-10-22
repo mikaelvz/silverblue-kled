416ad305e4379efb104fe0dcae467a738c271c69
mikaelvz <mikael@mvz.fr>
2024-10-22 14:08:20 +0200

    install Universal Blue tools

--- Containerfile
+++ Containerfile
@@ -4,6 +4,8 @@ FROM quay.io/fedora-ostree-desktops/silverblue:${RELEASE}
 
 COPY system /
 
+COPY --from=ghcr.io/ublue-os/config:latest /rpms /tmp/ublue-os/config/rpms
+
 RUN mkdir -p /var/lib/alternatives && \
     /tmp/build.sh && \
     ostree container commit
---
+++ system/tmp/scripts/ublue-tools.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Install Universal Blue tools
+echo "Installing Universal Blue tools"
+rpm-ostree install fzf \
+/tmp/ublue-os/config/rpms/ublue-os-update-services.noarch.rpm \
+/tmp/ublue-os/config/rpms/ublue-os-just.noarch.rpm \
+/tmp/ublue-os/config/rpms/ublue-os-luks.noarch.rpm
