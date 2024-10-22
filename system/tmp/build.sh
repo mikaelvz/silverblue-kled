58ecc5a568411d2baed056d25063fecb758c0255
mikaelvz <mikael@mvz.fr>
2024-10-22 12:33:59 +0200

    install packages

--- system/tmp/build.sh
+++ system/tmp/build.sh
@@ -13,6 +13,36 @@ echo "Installing RPM Fusion repositories"
 rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
     https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
+# Installing packages
+echo "Installing packages"
+rpm-ostree override remove \
+fdk-aac-free \
+mesa-va-drivers \
+ffmpeg-free \
+libavcodec-free \
+libavdevice-free \
+libavfilter-free \
+libavformat-free \
+libavutil-free  \
+libpostproc-free \
+libswresample-free \
+libswscale-free \
+--install=libheif-freeworld \
+--install=libheif-tools \
+--install=intel-media-driver \
+--install=gstreamer1-plugin-libav \
+--install=gstreamer1-plugins-bad-free-extras \
+--install=gstreamer1-plugins-bad-freeworld \
+--install=gstreamer1-plugins-ugly \
+--install=gstreamer1-vaapi \
+--install=ffmpeg \
+--install=ffmpeg-libs \
+--install=ffmpegthumbnailer \
+--install=pipewire-codec-aptx \
+--install=mesa-va-drivers-freeworld \
+--install=fdk-aac \
+--install=gnome-tweaks
+
 # Signing
 echo "Installing signing policy"
 rpm-ostree install yq
