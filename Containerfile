c7f0507fd4883b78d4d54b09022f3177f3a0f966
mikaelvz <mikael@mvz.fr>
2024-10-22 17:53:41 +0200

    add Universal Blue signing policy

--- Containerfile
+++ Containerfile
@@ -1,10 +1,10 @@
 ARG RELEASE=40
 
+FROM ghcr.io/ublue-os/config:latest AS ublue-os-config
+
 FROM quay.io/fedora-ostree-desktops/silverblue:${RELEASE}
-
 COPY system /
-
-COPY --from=ghcr.io/ublue-os/config:latest /rpms /tmp/ublue-os/config/rpms
+COPY --from=ublue-os-config /rpms /tmp/ublue-os/config/rpms
 
 RUN mkdir -p /var/lib/alternatives && \
     /tmp/build.sh && \
--- system/tmp/scripts/ublue-tools.sh
+++ system/tmp/scripts/ublue-tools.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 # Install Universal Blue tools
 echo "Installing Universal Blue tools"
 rpm-ostree install fzf \
-/tmp/ublue-os/config/rpms/ublue-os-update-services.noarch.rpm \
 /tmp/ublue-os/config/rpms/ublue-os-just.noarch.rpm \
-/tmp/ublue-os/config/rpms/ublue-os-luks.noarch.rpm
+/tmp/ublue-os/config/rpms/ublue-os-luks.noarch.rpm \
+/tmp/ublue-os/config/rpms/ublue-os-signing.noarch.rpm \
+/tmp/ublue-os/config/rpms/ublue-os-update-services.noarch.rpm
