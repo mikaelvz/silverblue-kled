919b241566c126d6fca79155255207ba38ca1fc4
mikaelvz <mikael@mvz.fr>
2024-10-22 12:03:01 +0200

    install RPM Fusion repositories

--- system/tmp/build.sh
+++ system/tmp/build.sh
@@ -8,6 +8,11 @@ export IMAGE_NAME="silverblue-kled"
 export IMAGE_REGISTRY="ghcr.io/mikaelvz"
 export MODULE_DIRECTORY="$(dirname "$0")/scripts"
 
+# Installing RPM Fusion repositories
+echo "Installing RPM Fusion repositories"
+rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
 # Signing
 echo "Installing signing policy"
 rpm-ostree install yq
