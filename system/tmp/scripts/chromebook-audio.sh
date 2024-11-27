8432cf40d4a91fc5d63216232e6354f855e98f2d
mikaelvz <mikael@mvz.fr>
2024-11-27 15:07:20 +0100

    test use specific chromebook audio commit

--- system/tmp/scripts/chromebook-audio.sh
+++ system/tmp/scripts/chromebook-audio.sh
@@ -6,4 +6,5 @@ set -euo pipefail
 echo "Installing chromebook audio"
 git clone https://github.com/WeirdTreeThing/chromebook-linux-audio /tmp/chromebook-linux-audio
 cd /tmp/chromebook-linux-audio
+git reset --hard 2002c72e108c99b822d94e1b383f68a00d7cca9d
 ./setup-audio -b kled
